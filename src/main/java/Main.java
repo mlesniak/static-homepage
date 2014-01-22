a120d023a541ad0fe0db07994adc4c382ac9165f
Michael Lesniak <mail@mlesniak.com>
2014-01-22 06:09:17 +0100

    Handling of dynamic files.
    - no error handling.
    - dirty code.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.DefaultHandler;
@@ -7,7 +9,13 @@ import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.handler.ResourceHandler;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.StringReader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 
 public class Main {
     String[] args;
@@ -84,6 +92,71 @@ public class Main {
     }
 
     private void createDynamicFiles(File sourceDirectory, File targetDirectory) {
+        File pages = new File(sourceDirectory.toPath() + "/_pages");
+        for (File file : FileUtils.listFiles(pages, null, false)) {
+            handleDynamicFile(sourceDirectory, targetDirectory, file);
+        }
+    }
 
+    private void handleDynamicFile(File sourceDirectory, File targetDirectory, File file) {
+        // Read configuration.
+        List<String> lines = null;
+        lines = readLines(file, lines);
+        Properties config = loadConfiguration(lines);
+
+        String layout = null;
+        try {
+            layout = FileUtils.readFileToString(
+                    new File(sourceDirectory.getPath() + "/_layout/"+ config.getProperty("layout") + ".html"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // Replace variables.
+        for (String key : config.stringPropertyNames()) {
+            layout = layout.replaceAll("\\$\\{" + key + "\\}", config.getProperty(key));
+        }
+
+        // Replace content.
+        layout = layout.replaceAll("\\$\\{content\\}", StringUtils.join(lines, "\n"));
+
+        try {
+            FileUtils.write(new File(targetDirectory.getPath() + "/" + file.getName()), layout);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    private List<String> readLines(File file, List<String> lines) {
+        try {
+            lines = IOUtils.readLines(new FileInputStream(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return lines;
+    }
+
+    private Properties loadConfiguration(List<String> lines) {
+        List<String> configuration = new LinkedList<>();
+        Iterator<String> iterator = lines.iterator();
+        while (iterator.hasNext()) {
+            String line = iterator.next();
+            configuration.add(line);
+            iterator.remove();
+            if (StringUtils.isBlank(line)) {
+                break;
+            }
+        }
+
+        Properties config = new Properties();
+        try {
+            config.load(new StringReader(StringUtils.join(configuration, "\n")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return config;
     }
 }
